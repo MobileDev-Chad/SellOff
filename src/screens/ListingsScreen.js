1d4102a814d8eb858ae9723e41d3cc85791f14cd
WebDev-Chad <chadwin.allotey@gmail.com>
2022-05-16 23:14:56 -0400

    update

--- src/screens/ListingsScreen.js
+++ src/screens/ListingsScreen.js
@@ -1,27 +1,38 @@
 import { FlatList, StyleSheet } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 
+import ActivityIndicator from '../components/ActivityIndicator'
+import Button from '../components/Button'
 import Card from '../components/Card'
 import listingsApi from '../api/listings'
 import routes from '../navigation/routes'
 import Screen from '../components/Screen'
+import Text from '../components/Text'
+import useApi from '../hooks/useApi'
 
 import { COLORS } from '../../constants/theme'
 
 export default ListingsScreen = ({ navigation }) => {
-  const [listings, setListings] = useState([])
+  const {
+    data: listings,
+    error,
+    loading,
+    request: loadListings,
+  } = useApi(listingsApi.getListings)
 
   useEffect(() => {
     loadListings()
   }, [])
 
-  const loadListings = async () => {
-    const response = await listingsApi.getListings()
-    setListings(response.data)
-  }
-
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <Text>Couldn't retrieve the listings.</Text>
+          <Button title="Retry" onPress={loadListings} />
+        </>
+      )}
+      <ActivityIndicator visible={loading} />
       <FlatList
         showsVerticalScrollIndicator={false}
         data={listings}
